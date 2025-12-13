69bf91d16c9219456e830e2f2fe8fcf9936d5d98
abhisheky1817 <abhisheky1817@gmail.com>
2025-12-13 19:53:19 +0530

    added Button component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Button from './Components/Buttons/Button';
+
+function App() {
+
+  return(
+    <div>
+      <Button />
+    </div>
+  )
+
+}
+
+export default App;
---
+++ src/Components/Buttons/Button.jsx
@@ -0,0 +1,12 @@
+function Button() {
+
+    return(
+        <button
+         className="px-4 py-2 bg-blue-500 border border-blue-700 text-white hover:bg-blue-700 rounded-md transition-all"
+         >
+            Click Me
+        </button>
+    )
+}
+
+export default Button;
